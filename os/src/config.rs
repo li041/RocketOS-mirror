d2284dcfe5c1095ed4a9cdab2f92b1551e464725
li041 <lxh050013@outlook.com>
2025-01-08 12:02:46 +0800

    support vm and index list

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,3 +1,6 @@
 
 
 pub const KERNEL_HEAP_SIZE: usize = 0x300_0000; // 48MB 
+pub const PAGE_SIZE: usize = 0x1000; // 4KB
+pub const KERNEL_BASE: usize = 0xffff_ffc0_0000_0000;
+pub const PAGE_SIZE_BITS: usize = 0xc;
